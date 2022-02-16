75d483054825934353e60fc2b6600184b07e05d1
Guillaume YE <guillaume.ye@epitech.eu>
2022-02-16 16:20:23 +0100

    Remove dead code in project

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { Routes, Route } from "solid-app-router";
 import Layout from "./components/Layout";
 
 const Menu = lazy(() => import("./pages/menu.jsx"));
-const Home = lazy(() => import("./pages/index.jsx"));
 
 function App() {
   return (
     <div>
       <Layout>
         <Routes>
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Menu />} />
         </Routes>
       </Layout>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,9 @@
-import NavItem from "./NavItem.jsx";
 import RestaurantInfo from "../components/RestaurantInfo";
 
 export default function Nav() {
   return (
     <nav>
       <RestaurantInfo />
-      <ul className="main-nav-list">
-        <NavItem text="Menu" />
-      </ul>
     </nav>
   );
 }
